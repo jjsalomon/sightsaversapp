1640e88c3fde23380c470533bfb8137d3d3e3938
Mark Twomey <mark2twomey@hotmail.com>
2017-03-26 16:32:06 +0100

    Donate Button Working

--- app/src/main/java/com/example/mark/sightsavers/MainActivity.java
+++ app/src/main/java/com/example/mark/sightsavers/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mark.sightsavers;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void donate(View view){
+        Intent donateIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://donate.sightsavers.org/smxpatron/ireland/donate.html"));
+        startActivity(donateIntent);
     }
 }
